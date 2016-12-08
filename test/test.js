d44477b217e27f3dbaf75372f562078adf5a0aa4
Raymond Luong <raymond.n.luong@gmail.com>
2016-12-07 15:53:27 -1000

    Test skeleton

--- test/test.js
+++ test/test.js
@@ -1,8 +1,21 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+var Stack = require('../lib/Stack.js')
+// var assert = require('assert');
+var should = require('chai').should();
+
+describe('Stack', function() {
+
+  describe('push()', function() {
+    it('should have a push function', function() {
+      var stack = new Stack;
+      stack.hello.should.be.a('function')
     });
   });
+
+  describe('pop()', function() {
+    it('should have a pop function', function() {
+      var stack = new Stack;
+      stack.hello.should.be.a('function')
+    });
+  });
+
 });
